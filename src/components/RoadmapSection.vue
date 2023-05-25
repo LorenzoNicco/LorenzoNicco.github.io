660e3190678b4b855c244f2f5a088100b2d9d74b
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-05-25 09:48:41 +0200

    Add modali in roadmap

--- src/components/RoadmapSection.vue
+++ src/components/RoadmapSection.vue
@@ -9,15 +9,42 @@
 
 <template>
     <div class="col-8 mb-5">
-        <h4>{{ data.company }}</h4>
+        <h4 data-bs-toggle="modal" :data-bs-target="'#'+data.id">{{ data.company }}</h4>
 
-        <h5>{{ data.title }}</h5>
+        <!-- Modal -->
+        <div class="modal fade" :id="data.id" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h2 class="modal-title fs-5" id="exampleModalLabel">{{ data.title }}</h2>
+
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+
+                    <div class="modal-body">
+                        <h5>🏢 {{ data.company }}</h5>
+
+                        <p>🗺 {{ data.location }}</p>
+
+                        <p>⏳ {{ data.duration }}</p>
+
+                        <p>❔ {{ data.description }}</p>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- <h5>{{ data.title }}</h5>
 
         <p>{{ data.location }}</p>
 
         <p>{{ data.duration }}</p>
 
-        <p>{{ data.description }}</p>
+        <p>{{ data.description }}</p> -->
     </div>
 </template>
 
--- src/pages/AppBio.vue
+++ src/pages/AppBio.vue
@@ -10,6 +10,7 @@
                 data: {
                     experiences: [
                         {
+                            id: 'Boolean-experience',
                             company: 'Boolean',
                             title: 'Jr Full Stack Web Developer Trainee',
                             location: 'Remote',
@@ -17,36 +18,17 @@
                             description: 'Corso teorico/pratico della durata di 6 mesi, durante il quale mi sono cimentato nella realizzazione di progetti ispirati a siti e web app realmente esistenti (ad esempio: WatsApp web, Spotify e Dropbox). I progetti realizzati durante il corso presentavano una difficoltà sempre crescente e delle scadenze variabili.'
                         },
                         {
+                            id: 'The-Focus',
                             company: 'The Focus',
                             title: '3D DMP Trainee',
                             location: 'Remote',
                             duration: 'Settembre 2021 - Novembre 2021',
                             description: 'Corso teorico/pratico di 2 mesi durante il quale ho realizzato vari progetti di matte painting in 2d e 3d. Per la realizzazione di questi ho sfruttato i programmi Photoshop, Maya e Nuke'
                         },
-                        {
-                            company: 'Esselunga spa',
-                            title: 'Operaio',
-                            location: 'Pistoia, Prato',
-                            duration: 'Novembre 2017 - Ottobre 2021',
-                            description: 'Addetto al reparto Frutta e Verdura.'
-                        },
-                        {
-                            company: 'Grom - Il gelato come una volta',
-                            title: 'Conista Junior',
-                            location: 'Firenze',
-                            duration: 'Luglio 2017 - Novembre 2017',
-                            description: 'Esperienza svolta in gelateria a Firenze, dove ho svolto servizio diretto alla clientela, sia locale che straniera.'
-                        },
-                        {
-                            company: 'Poste Italiane spa',
-                            title: 'Postino',
-                            location: 'Prato, Pistoia',
-                            duration: 'Febbraio 2015 - Gennaio 2017',
-                            description: 'Consegna di posta e pacchi nei comuni di Prato, Pistoia, Montemurlo, Vaiano e Vernio'
-                        },
                     ],
                     education: [
                         {
+                            id: 'Boolean-education',
                             company: 'Boolean',
                             title: 'Master in Web Development',
                             location: 'Remoto',
@@ -54,6 +36,7 @@
                             description: 'Percorso di studi intensivo di 700 ore, finalizzato all\'apprendimento degli strumenti necessari allo sviluppo di siti e applicazioni web.'
                         },
                         {
+                            id: 'CG-Spectrum-diploma',
                             company: 'CG Spectrum - College of Digital Art and Animation',
                             title: 'Advanced Concept Art',
                             location: 'Remoto',
@@ -61,6 +44,7 @@
                             description: 'Percorso di studi intensivo di 700 ore, finalizzato all\'apprendimento degli strumenti necessari allo sviluppo di siti e applicazioni web.' //correggere
                         },
                         {
+                            id: 'CG-Spectrum-certificate',
                             company: 'CG Spectrum - College of Digital Art and Animation',
                             title: 'Introduction to Concept Art & Illustration',
                             location: 'Remoto',
@@ -68,6 +52,7 @@
                             description: 'Percorso di studi intensivo di 700 ore, finalizzato all\'apprendimento degli strumenti necessari allo sviluppo di siti e applicazioni web.' //correggere
                         },
                         {
+                            id: 'Liceo-Petrocchi',
                             company: 'Liceo Artistico P. Petrocchi',
                             title: 'Diploma in Architettura e Arredamento',
                             location: 'Pistoia',
